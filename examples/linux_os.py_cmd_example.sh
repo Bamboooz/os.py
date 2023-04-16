2397d3d3bd3743bd0130c70ce0913eec5cf7ae76
Bamboooz <bambusixmc@gmail.com>
2023-04-16 14:59:22 +0200

    added examples, remove old Linux test files

---
+++ examples/linux_os.py_cmd_example.sh
@@ -0,0 +1,5 @@
+# return the os.py information to the command prompt
+os_py --dump=cmd
+
+# dump the os.py information output to a .txt file
+os_py --dump=txt --file=C:\Users\User\Desktop\log.txt
---
+++ examples/windows_os.py_cmd_example.bat
@@ -0,0 +1,5 @@
+REM return the os.py information to the command prompt
+os_py --dump=cmd
+
+REM dump the os.py information output to a .txt file
+os_py --dump=txt --file=C:\Users\User\Desktop\log.txt
