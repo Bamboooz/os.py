1833c70792822a11da0fd88131d7899c6d5f620b
Bamboooz <bambusixmc@gmail.com>
2023-07-06 01:28:54 +0200

    os.py constants placeholders

---
+++ _ospywin.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2022, Bamboooz
+# All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# here will be constants for the os.py library
