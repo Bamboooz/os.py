c0e993433ee96cfbec24008e0fa3e039f5a53215
Bamboooz <bambusixmc@gmail.com>
2023-07-05 21:11:05 +0200

    add __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from ospylib.system.arch.windows import sys
-from ospylib.hardware.arch.windows import graphics as gpu
-from ospylib.ospy_windows import baseboard, memory
+import system
+import storage
+import sensors
+import hardware
+import drivers
+import display
+import device
+import battery
