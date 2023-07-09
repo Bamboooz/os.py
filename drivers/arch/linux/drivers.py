b791c9ef96c77a7a03aee74a0bf7132301858767
Bamboooz <bambusixmc@gmail.com>
2023-07-09 16:36:38 +0200

    enchance driver information on Linux

--- drivers/arch/linux/drivers.py
+++ drivers/arch/linux/drivers.py
@@ -5,25 +5,104 @@
 
 import os
 
+from collections import namedtuple
 
-def get_drivers() -> dict:
-    driver_locations = {}
+from common.prompt.prompt import execute_command
 
-    # Search for driver files in the specified directory
-    driver_dirs = os.listdir('/sys/bus/')
 
-    # Iterate over driver directories
-    for driver_dir in driver_dirs:
-        driver_path = f'/sys/bus/{driver_dir}/drivers/'
+def driver_query(driver: str=None) -> namedtuple or dict:
+    """
+    Returns a dict of drivers and information about them if driver not specified.
+    {
+        '<driver_name>': '<file_path>',
+        ...
+    }
+    If driver name or driver path is specified, returns a named tuple with items:
+        - name: Driver's name
+        - path: Path to the driver file
+        - description: Driver's description
+    """
+    if driver is not None and os.path.isfile(driver):
+        return _get_data_from_path(driver)
+    elif driver is not None:
+        return _get_data_from_name(driver)
 
-        # Check if the driver directory exists
-        if os.path.exists(driver_path):
-            drivers = os.listdir(driver_path)
+    kernel_release = execute_command('uname -r', 0)[0]
+    driver_directory = f"/lib/modules/{kernel_release}/kernel/drivers/"
+    driver_paths = [os.path.join(root, file) for root, dirs, files in os.walk(driver_directory) for file in files]
+    drivers = [os.path.splitext(os.path.basename(file_path))[0] for file_path in driver_paths]
 
-            # Iterate over drivers within the directory
-            for driver in drivers:
-                driver_name = driver
-                driver_location = os.path.join(driver_path, driver)
-                driver_locations[driver_name] = driver_location
+    driver_data = _get_driver_data(drivers, driver_paths)
 
-    return driver_locations
+    if driver is None and driver_data is not None:
+        return {key: value for key, value in driver_data.items()}
+
+    driver_data_format = namedtuple('driver_data_format', ['path', 'name', 'description'])
+    driver_not_found = driver_data_format(path=None, name=None, description=None)
+    return driver_not_found
+
+
+def _get_driver_data(drivers: list, driver_paths: list) -> dict or None:
+    """
+    Put driver names and driver filepaths in a sorted dictionary.
+    """
+    try:
+        driver_info = {
+            driver_name: next((item for item in driver_paths if f'/{driver_name}.ko' in item), None)
+            for driver_name in drivers
+        }
+
+        return {key: value for key, value in driver_info.items() if value is not None}  # Remove not found values
+    except:
+        return None
+
+
+def _get_data_from_name(driver: str) -> namedtuple:
+    """
+    Get driver information from driver name.
+    """
+    driver_data_format = namedtuple('driver_data_format', ['path', 'name', 'description'])
+
+    try:
+        driver_data = execute_command(f'modinfo {driver}', 0)
+
+        driver_path = None
+        driver_description = None
+
+        for line in driver_data:
+            if line.startswith('filename:'):
+                driver_path = line.replace('filename:', '').strip()
+            elif line.startswith('description:'):
+                driver_description = line.replace('description:', '').strip()
+
+        if driver_path is not None and driver_description is not None:
+            return driver_data_format(description=driver_description, path=driver_path, name=driver)
+        else:
+            raise Exception("Failed to get driver information.")
+    except:
+        return driver_data_format(description=None, path=None, name=None)
+
+
+def _get_data_from_path(driver_path: str) -> namedtuple:
+    """
+    Get driver information from driver filepath.
+    """
+    driver_data_format = namedtuple('driver_data_format', ['path', 'name', 'description'])
+
+    try:
+        driver = os.path.splitext(os.path.basename(driver_path))[0]
+
+        driver_data = execute_command(f'modinfo {driver}', 0)
+
+        driver_description = None
+
+        for line in driver_data:
+            if line.startswith('description:'):
+                driver_description = line.replace('description:', '').strip()
+
+        if driver is not None and driver_description is not None:
+            return driver_data_format(description=driver_description, path=driver_path, name=driver)
+        else:
+            raise Exception("Failed to get driver information.")
+    except:
+        return driver_data_format(description=None, path=None, name=None)
