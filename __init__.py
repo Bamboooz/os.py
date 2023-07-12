bab0400bba6f47bf33edab6fa1f9ebc654015776
Bamboooz <bambusixmc@gmail.com>
2023-07-12 21:17:10 +0200

    include new modules in __init__.py 🪐

--- __init__.py
+++ __init__.py
@@ -3,13 +3,20 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import system
-import storage
-import sensors
-import hardware
-import drivers
-import display
-import device
-import battery
-import audio
-import peripherals
+from system import *
+from storage import *
+from sensors import *
+from hardware import *
+from drivers import *
+from display import *
+from device import *
+from audio import *
+from peripherals import *
+from firmware import *
+from process import *
+
+from sensors.battery import *
+from sensors.fans import *
+from sensors.usage import *
+from sensors.voltage import *
+from sensors.temperature import *
