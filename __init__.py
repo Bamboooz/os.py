e032d8047edab86662e77ec4be637d54cfb20a80
Bamboooz <bambusixmc@gmail.com>
2023-07-14 20:42:04 +0200

    block using ospylib on not supported operating systems

--- __init__.py
+++ __init__.py
@@ -5,7 +5,6 @@
 
 from system import *
 from storage import *
-from sensors import *
 from drivers import *
 from display import *
 from device import *
@@ -25,3 +24,14 @@ from sensors.fans import *
 from sensors.usage import *
 from sensors.voltage import *
 from sensors.temperature import *
+
+
+__author__ = "Bamboooz"
+__repo__ = "https://github.com/Bamboooz/os.py"
+__version__ = "0.0.1"
+__supported_os__ = ['Windows', 'Linux']
+__os__ = name()
+
+
+if __os__ not in __supported_os__:
+    raise OSError(f"Unsupported by ospylib operating system: {__os__}.")
